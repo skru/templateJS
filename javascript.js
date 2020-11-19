86b76d3b6e99fe31c4c7c0b8d19ea12e1cad2f0c
joseph.mohan@nodeweave.com <joseph.mohan@nodeweave.com>
2020-11-19 08:53:21 +0000

    + navbar burger, dropdown complete. experimenting with modals

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
-// navbar
-$("body").on("click", ".navbar-burger", function () {
-  $(".navbar-burger").toggleClass("is-active");
-  $(".navbar-menu").toggleClass("is-active");
+// navbar burger
+$("body").on("click", "[data-zcc-block='navbar'] .navbar-burger", function () {
+  $(this).toggleClass("is-active");
+  var wrapper = $(this).parents("[data-zcc-block='navbar']")
+  wrapper.find('.navbar-menu').toggleClass("is-active");
 });
+// navbar dropdown
+$("body").on("click", "[data-zcc-block='navbar'] .has-dropdown", function () {
+  $(this).toggleClass("is-active");
+});
+// image-modal-open
+$("body").on("click", "[data-zcc-block='image-modal'] img", function () {
+  var wrapper = $(this).parents("[data-zcc-block='image-modal']")
+  wrapper.find('.modal').toggleClass("is-active");
+});
+// image-modal-close
+$("body").on("click", "[data-zcc-block='image-modal'] button", function () {
+  var wrapper = $(this).parents("[data-zcc-block='image-modal']")
+  wrapper.find('.modal').toggleClass("is-active");
+});
+
