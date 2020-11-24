2445b62f044dee2a7d65e8931e39765b3c98d2e0
joseph.mohan@nodeweave.com <joseph.mohan@nodeweave.com>
2020-11-24 10:09:30 +0000

    + form validation. switched to only using data attributes rather than class selectors

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
 // navbar burger
-$("body").on("click", "[data-zcc-block='navbar'] .navbar-burger", function () {
+$("body").on("click", "[data-zcc-block='navbar'] [data-zcc-block='navbar-burger']", function () {
   $(this).toggleClass("is-active");
   var wrapper = $(this).parents("[data-zcc-block='navbar']")
-  wrapper.find('.navbar-menu').toggleClass("is-active");
+  wrapper.find("[data-zcc-block='navbar-menu']").toggleClass("is-active");
 });
 // navbar dropdown
-$("body").on("click", "[data-zcc-block='navbar'] .has-dropdown", function (event) {
+$("body").on("click", "[data-zcc-block='navbar'] [data-zcc-block='navbar-dropdown']", function (event) {
   // prevent clicking on items closing menu
-  if ($(event.target).hasClass('navbar-link')) {
+  console.log("cl")
+  if ($(event.target).attr('data-zcc-block') === "navbar-link") {
     $(this).toggleClass("is-active");
     // close any other open menus
-    var openMenus = $("body").find(".navbar-menu .is-active").not(this);
+    var openMenus = $("body").find("[data-zcc-block='navbar-menu'] .is-active").not(this);
+    console.log("opens", openMenus)
     openMenus.each(function () {
       $(this).toggleClass("is-active");
     });
@@ -29,12 +31,12 @@ $("body").on("click", "[data-zcc-block='image-modal'] button", function () {
 });
 
 // tabs
-$("body").on("click", "[data-zcc-block='tabs'] .bulma-tab", function (event) {
+$("body").on("click", "[data-zcc-block='tabs'] [data-zcc-block='tab']", function (event) {
   var tabIndex = $(this).attr("data-zcc-tab");
   var wrapper = $(this).parents("[data-zcc-block='tabs']")
-  wrapper.find('.bulma-tab').removeClass("is-active");
+  wrapper.find("[data-zcc-block='tab']").removeClass("is-active");
   $(this).toggleClass("is-active");
-  var content = wrapper.find(".tab-content-wrapper")
+  var content = wrapper.find("[data-zcc-block='tab-content']")
   $(content).each(function (index) {
     if (parseInt(tabIndex) === index) {
       $(this).show();
@@ -45,21 +47,33 @@ $("body").on("click", "[data-zcc-block='tabs'] .bulma-tab", function (event) {
 });
 
 // forms
-$("body").on("click", "form :submit", function (event) {
-  event.preventDefault();
-  var wrapper = $(this).parents("form")
-  console.log("serialised", $(wrapper).serialize());
-  $.ajax({
-    url: this.http,
-    type: 'POST',
-    data: $(wrapper).serialize(),
-    success: function (data) {
-      console.log(data)
-      // show notification
-    },
-    error: function (e) {
-      console.log(e.message);
+$("body").on("click", "[data-zcc-block='form'] [data-zcc-block='form-submit']", function (event) {
+  event.preventDefault(); // prevent normal form submission
+  var isValid = true;
+  var wrapper = $(this).parents("[data-zcc-block='form']")
+  var fields = $(wrapper).find("[data-zcc-block='form-field']");
+  fields.each(function (index) {
+    // get input and errors for each field
+    var input = $(this).find("[data-zcc-block='form-field-input']")
+    var errors = $(this).find("[data-zcc-block='form-field-error']")
+    errors.html(""); // reset any errors
+    if (!input[0].checkValidity()) { // check validity
+      isValid = false;
+      errors.html(input[0].validationMessage);
     }
   });
+  if (isValid) {
+    $.ajax({
+      url: this.http,
+      type: 'POST',
+      data: $(wrapper).serialize(),
+      success: function (data) {
+        console.log(data)
+        // show notification
+      },
+      error: function (e) {
+        console.log(e.message);
+      }
+    });
+  }
 });
-
